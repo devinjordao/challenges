f63df230a187c006681fba9f89bef4f61225d5f9
Devin Jordao <devinjordao@gmail.comgit config --global user.name Devin>
2019-08-07 19:09:10 +0000

    Blur challenge 1

---
+++ blur_1.rb
@@ -0,0 +1,24 @@
+class Image
+  def initialize(pic)
+    @image = pic
+  end
+
+  def output_image
+    @image.each do |output|
+      puts output.each { |p| p }.join
+    end
+  end
+end
+
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+image.output_image
+
+@image.push(2)
+
+image.output_image
